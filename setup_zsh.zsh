3f133dd80a4cb916150518ce69160879f3d4a143
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-05-05 21:41:12 +0200

    Redirect output to blackhole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,4 +8,4 @@ echo "Enter superuser (sudo) password to edit /etc/shells"
 echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
 
 echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+chsh -s '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
