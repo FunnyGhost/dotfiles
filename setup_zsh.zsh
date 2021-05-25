dc784cf40297b3c7df3a5aef664a326c0a18bbeb
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-05-25 22:43:48 +0200

    Added explanatory comment

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,6 +4,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation not needed since zsh it's already installed by brew
 
+# We're doing this because we want to give sudo permission only to the tee command, not to the whole shell.
 echo "Enter superuser (sudo) password to edit /etc/shells"
 echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
 
