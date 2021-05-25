20bcfd46d918afda4b45c27eba8876ae7d79d4da
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-05-25 23:07:46 +0200

    Setup zsh as defaul interactive and not-interactive shell

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,9 +4,23 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation not needed since zsh it's already installed by brew
 
-# We're doing this because we want to give sudo permission only to the tee command, not to the whole shell.
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
