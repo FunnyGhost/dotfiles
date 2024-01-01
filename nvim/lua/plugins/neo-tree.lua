857bee4c7e7d32027faacfc6d6368696048a70d4
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-01-01 20:47:10 +0100

    update neotree shortcuts

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,6 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right toggle<CR>", {})
 	end,
 }
