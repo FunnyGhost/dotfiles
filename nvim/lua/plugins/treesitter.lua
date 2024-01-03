c13f1118e097a1710fabb1ddaf70614505f23f14
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-01-03 13:48:02 +0100

    enabled incremental selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -20,6 +20,15 @@ return {
 			},
 			highlight = { enable = true },
 			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
 		})
 	end,
 }
