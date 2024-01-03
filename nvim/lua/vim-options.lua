3007ce3d5c8e9079c98b919916a5c7fb25e40ee1
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-01-03 16:34:12 +0100

    use tabs and update vim settings

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,8 +1,16 @@
-vim.cmd("set expandtab")
+vim.cmd("set noexpandtab")
+vim.cmd("set autoindent")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Set <leader> as <space>
 vim.g.mapleader = " "
-
+-- Show line numbers
 vim.wo.number = true
-
+-- Unify clipboard with the system
+vim.o.clipboard = "unnamedplus"
+-- Keep unde history
+vim.o.undofile = true
+-- Set a better completion experience
+vim.o.completeopt = "menu,menuone,preview,noselect"
