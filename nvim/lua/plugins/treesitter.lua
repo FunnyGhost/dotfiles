94c2d65a77951a66ef2141cc5c82e22dd049aa78
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-01-03 22:45:55 +0100

    add treesitter text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,45 @@
 -- This highlights the whole document
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local config = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
+	},
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local config = require("nvim-treesitter.configs")
 
-    config.setup({
-      auto_install = true,
-      ensure_installed = {
-        "lua",
-        "javascript",
-        "html",
-        "bash",
-        "gitignore",
-        "prisma",
-        "tsx",
-        "typescript",
-        "css",
-        "vim",
-        "vimdoc"
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+		config.setup({
+			auto_install = true,
+			ensure_installed = {
+				"javascript",
+				"typescript",
+				"json",
+				"html",
+				"tsx",
+				"css",
+				"yaml",
+				"prisma",
+				"markdown",
+				"dockerfile",
+				"gitignore",
+				"bash",
+				"vim",
+				"vimdoc",
+				"lua",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
