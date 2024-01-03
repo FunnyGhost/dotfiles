470e973c6f6ea058ef6c4b1810ceb0a4a16003d6
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-01-03 13:47:35 +0100

    updated lsp configuration

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -12,18 +12,26 @@ return {
 		lazy = false,
 		opts = {
 			auto_install = true,
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"emmet_ls",
+				"cssls",
+				"lua_ls",
+			},
 		},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+		},
 		config = function()
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
 			})
@@ -31,14 +39,48 @@ return {
 				capabilities = capabilities,
 			})
 
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig["emmet_ls"].setup({
+				capabilities = capabilities,
+			})
+			-- configure lua server (with special settings)
+			lspconfig["lua_ls"].setup({
+				capabilities = capabilities,
+				settings = { -- custom settings for lua
+					Lua = {
+						-- make the language server recognize "vim" global
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							-- make language server aware of runtime files
+							library = {
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.stdpath("config") .. "/lua"] = true,
+							},
+						},
+					},
+				},
+			})
+
+			-- Change the Diagnostic symbols in the sign column (gutter)
+			-- (not in youtube nvim video)
+			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }
