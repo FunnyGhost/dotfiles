c4feb50fea438d287dc13c742bfee4b36e8d6c98
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-02-06 12:15:10 +0100

    set-up twilight for area highlight

---
+++ nvim/lua/plugins/twilight.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/twilight.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>T", ":Twilight<CR>", { desc = "Toggle [T]wilight" })
+	end,
+}
