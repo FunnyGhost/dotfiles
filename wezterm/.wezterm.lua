6b5ddca2e7abb71458fec32a43a58a2ab241b6a9
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-03-09 22:36:24 +0100

    basic wezterm setup

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,12 @@
+-- pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- this will hold the configuration
+local config = wezterm.config_builder()
+
+-- this is where you actually apply your config choices
+
+-- changing the color scheme looks like this
+config.color_scheme = "Batman"
+
+return config
