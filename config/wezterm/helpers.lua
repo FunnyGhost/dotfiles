508bf993450213a6e63997223cbbe5fa6faea931
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-03-22 13:45:10 +0100

    use wezterm multiplexer

---
+++ config/wezterm/helpers.lua
@@ -0,0 +1,116 @@
+
+local wezterm = require 'wezterm'
+
+-- Integration with neovim panes
+local function isViProcess(pane)
+  -- get_foreground_process_name On Linux, macOS and Windows,
+  -- the process can be queried to determine this path. Other operating systems
+  -- (notably, FreeBSD and other unix systems) are not currently supported
+  -- return pane:get_foreground_process_name():find('n?vim') ~= nil
+  -- Use get_title as it works for multiplexed sessions too
+  return pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+  local vim_pane_changed = false
+
+  if isViProcess(pane) then
+    local before = pane:get_cursor_position()
+    window:perform_action(
+    -- This should match the keybinds you set in Neovim.
+      wezterm.action.SendKey({ key = vim_direction, mods = 'CTRL' }),
+      pane
+    )
+    wezterm.sleep_ms(100)
+    local after = pane:get_cursor_position()
+
+    if before.x ~= after.x and before.y ~= after.y then
+      vim_pane_changed = true
+    end
+  end
+
+  if not vim_pane_changed then
+    window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
+  end
+end
+
+wezterm.on('ActivatePaneDirection-right', function(window, pane)
+  conditionalActivatePane(window, pane, 'Right', 'l')
+end)
+wezterm.on('ActivatePaneDirection-left', function(window, pane)
+  conditionalActivatePane(window, pane, 'Left', 'h')
+end)
+wezterm.on('ActivatePaneDirection-up', function(window, pane)
+  conditionalActivatePane(window, pane, 'Up', 'k')
+end)
+wezterm.on('ActivatePaneDirection-down', function(window, pane)
+  conditionalActivatePane(window, pane, 'Down', 'j')
+end)
+
+-- Tab titles
+wezterm.on("update-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = "#f7768e"
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = "#7dcfff"
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = "#bb9af7"
+  end
+
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+
+  -- Current working directory
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    if type(cwd) == "userdata" then
+      -- Wezterm introduced the URL object in 20240127-113634-bbcac864
+      cwd = basename(cwd.file_path)
+    else
+      -- 20230712-072601-f4abf8fd or earlier version
+      cwd = basename(cwd)
+    end
+  else
+    cwd = ""
+  end
+
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+  cmd = cmd and basename(cmd) or ""
+
+  -- Time
+  local time = wezterm.strftime("%H:%M")
+
+  -- Left status (left of the tab line)
+  window:set_left_status(wezterm.format({
+    { Foreground = { Color = stat_color } },
+    { Text = "  " },
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " |" },
+  }))
+
+  -- Right status
+  window:set_right_status(wezterm.format({
+    -- Wezterm has a built-in nerd fonts
+    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = "#e0af68" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+    { Text = "  " },
+  }))
+end)
+
+
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,12 +1,63 @@
 -- pull in the wezterm API
 local wezterm = require("wezterm")
+local helpers = require("helpers")
 
 -- this will hold the configuration
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- this is where you actually apply your config choices
+config.font = wezterm.font_with_fallback({
+  { family = "CaskaydiaCove Nerd Font Mono", scale = 1.1 },
+})
+config.line_height = 1.2
 
 -- changing the color scheme looks like this
 config.color_scheme = "Eldritch"
+
+-- window stuff
+config.window_decorations = "RESIZE"
+config.scrollback_lines = 3500
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5
+}
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+
+-- keys
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  -- Pane keybindings
+  { key = "-", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "|", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "m", mods = "LEADER", action = act.TogglePaneZoomState },
+  -- Integration with neovim panes
+  { key = 'h', mods = 'CTRL',   action = wezterm.action.EmitEvent('ActivatePaneDirection-left') },
+  { key = 'j', mods = 'CTRL',   action = wezterm.action.EmitEvent('ActivatePaneDirection-down') },
+  { key = 'k', mods = 'CTRL',   action = wezterm.action.EmitEvent('ActivatePaneDirection-up') },
+  { key = 'l', mods = 'CTRL',   action = wezterm.action.EmitEvent('ActivatePaneDirection-right') },
+  -- Tab keybindings
+  { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+  { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+  { key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+  {
+    key = "r",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
+}
 
 return config
