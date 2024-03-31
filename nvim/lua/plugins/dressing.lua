ee6d53dee7c930ee81193bff3ddbbfc58d8929d1
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-03-31 21:17:21 +0200

    moved configuration into the config folder

--- nvim/lua/plugins/dressing.lua
+++
@@ -1,5 +0,0 @@
--- Plugin that shows a pop-up when input is needed
-return {
-	"stevearc/dressing.nvim",
-	event = "VeryLazy",
-}
--- nvim/lua/plugins/lualine.lua
+++
@@ -1,4 +0,0 @@
--- The line at the bottom of the screen, showing the status and a bunch of other extra information
-return {
-  "nvim-lualine/lualine.nvim",
-}
