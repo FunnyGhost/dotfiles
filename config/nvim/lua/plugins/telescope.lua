be0dc481a96225c48ac7af88960af76ee5ac0315
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-04-19 22:22:18 +0200

    added todo-comments

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,16 +2,19 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+    dependencies = { "nvim-lua/plenary.nvim", "folke/todo-comments.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
     config = function()
       local telescope = require("telescope")
       local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind [g]rep" })
-      vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [g]rep" })
+      vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "[F]ind [s]ymbol under cursor" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[F]ind [r]ecently opened files" })
       vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind [g]rep" })
+      vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "[F]ind [t]odo comments" })
 
       telescope.setup({
         defaults = {
