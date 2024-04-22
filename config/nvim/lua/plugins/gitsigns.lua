bc578c2b21f61ceb3bda86e77352bd9c4b65608f
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-04-22 12:01:17 +0200

    setup gitsigns with shortcuts

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,47 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("gitsigns").setup()
-	end,
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
+
+      -- Navigation
+      map("n", "]h", gs.next_hunk, "Next [h]unk")
+      map("n", "[h", gs.prev_hunk, "Prev [h]unk")
+
+      -- Actions
+      map("n", "<leader>hs", gs.stage_hunk, "[h]unk [s]tage")
+      map("n", "<leader>hr", gs.reset_hunk, "[h]unk [r]eset")
+      map("v", "<leader>hs", function()
+        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "[h]unk [s]tage")
+      map("v", "<leader>hr", function()
+        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "[h]unk [r]eset")
+
+      map("n", "<leader>hS", gs.stage_buffer, "[h]unk [S]tage buffer")
+      map("n", "<leader>hR", gs.reset_buffer, "[h]unk [R]eset buffer")
+
+      map("n", "<leader>hu", gs.undo_stage_hunk, "[h]unk [u]ndo stage")
+
+      map("n", "<leader>hp", gs.preview_hunk, "[h]unk [p]review")
+
+      map("n", "<leader>hb", function()
+        gs.blame_line({ full = true })
+      end, "[h]unk [b]lame line")
+      map("n", "<leader>hB", gs.toggle_current_line_blame, "[h]unk [B]lame line")
+
+      map("n", "<leader>hd", gs.diffthis, "[h]unk [d]iff this")
+      map("n", "<leader>hD", function()
+        gs.diffthis("~")
+      end, "[h]unk [D]iff this")
+
+      -- Text object
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
+    end,
+  },
 }
--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -12,7 +12,7 @@ return {
       javascriptreact = { "eslint_d" },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroups("lint", { clear = true })
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
@@ -20,7 +20,7 @@ return {
       end,
     })
 
-    vim.keymay.set("n", "<leader>l", function()
+    vim.keymap.set("n", "<leader>l", function()
       lint.try_lint()
     end, { desc = 'Trigger [l]inting for the current file' })
   end
