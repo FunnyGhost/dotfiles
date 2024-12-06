c0c6357cff1f57f864bf639bb52455b8b4a9c881
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-12-06 16:24:13 +0100

    Use eslint instead of eslint_d

--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -6,10 +6,10 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
+      javascript = { "eslint" },
+      typescript = { "eslint" },
+      typescriptreact = { "eslint" },
+      javascriptreact = { "eslint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
