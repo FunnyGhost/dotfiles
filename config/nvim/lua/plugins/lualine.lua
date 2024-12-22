75d6d615f542ae0dafde404bb24ec22103ca0081
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-12-22 18:48:54 +0100

    Try mini.statusline instead of lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,13 @@
 -- The line at the bottom of the screen, showing the status and a bunch of other extra information
-return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "eldritch",
-      },
-    })
-  end,
-}
+return {}
+-- return {
+-- 	"nvim-lualine/lualine.nvim",
+-- 	enable = true,
+-- 	config = function()
+-- 		require("lualine").setup({
+-- 			options = {
+-- 				theme = "eldritch",
+-- 			},
+-- 		})
+-- 	end,
+-- }
