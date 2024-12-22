778c11982c7dabb9b3b940edf7ccf0e3b134ddfd
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2024-12-22 18:49:34 +0100

    Added description

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -34,13 +34,13 @@ vim.opt.termguicolors = true -- True color support
 
 -- Commands
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight on yank",
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
