2d3034f2524b76ca1d1cd3e01a21561f0e6cee69
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2025-08-08 17:22:05 +0200

    feat(nvim): add LazyGit integration
    - Add LazyGit plugin with plenary.nvim dependency
    - Map <leader>lg to open LazyGit
    - Lazy load on command use

---
+++ config/nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,22 @@
+-- =====================================================
+-- LAZYGIT INTEGRATION
+-- =====================================================
+-- Fast and powerful Git UI for Neovim
+
+return {
+	"kdheepak/lazygit.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{
+			"<leader>lg",
+			"<cmd>LazyGit<CR>",
+			desc = "[G]it lazy[G]it",
+		},
+	},
+	-- Optional: Set custom LazyGit config path
+	-- init = function()
+	--   vim.g.lazygit_config = vim.fn.expand("~/.config/lazygit/config.yml")
+	-- end,
+}
